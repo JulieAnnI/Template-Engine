fe71923c20ca970b2d9e338b0354d8d98b113c60
Julie Ann <julieanniwinski@gmail.com>
2021-02-09 12:24:54 -0500

    Refactored inquirer questions

--- app.js
+++ app.js
@@ -7,66 +7,102 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+const ADD = "Add Team Member", EXIT = "Exit";
 const render = require("./lib/htmlRenderer");
+const { exit } = require("process");
 
-//Questions
-const questions = [
-    {
-        type:"input",
-        name: "name",
-        message: "What is your name?"
-    },
-    {
-        type:"input",
-        name: "id",
-        message: "What is your id number?"
-    },
-    {
-        type:"input",
-        name: "email",
-        message: "What is your email address?"
-    },
-    {
-        type:"list",
-        name: "role",
-        message: "What is your role?"
-        answers: ["Manager", "Engineer", "Intern"]
-    },
-    {
-        when: (answers) => {                     
-            if (answers.role === "Engineer") {
-                return true;
-            }
-        },
-        type: "input",
-        name: "github",
-        message: "Enter the employee's GitHub username."
-    },
-    {
-        when: (answers) => {                     
-            if (answers.role === "Manager") {
-                return true;
-            }
-        },
-        type: "input",
-        name: "officeNumber",
-        message: "Enter office number."
-    },
-    {
-        when: (answers) => {                     
-            if (answers.role === "Intern") {
-                return true;
-            }
-        },
-        type: "input",
-        name: "school",
-        message: "Enter the employee's school."
-    },
 
-];
+init();
 
+//Initialize Function
+async function generatorPrompt() {
+console.log("Team Generator Main Menu");
+    try {
+        let answer = await inquirer.prompt({
+            name: "addOrExit",
+            type: "list",
+            message: "Would you like to add a Team Member or Exit?",
+            choices: [ADD, EXIT]
+        });
+
+        switch (answer.addOrExit) {
+
+            case ADD:
+                return add();
+            case EXIT:
+                return exit();
+
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+//Add Team Member
+async function add() {
+    console.log("Add Team Member")
+    try {
+        let answer = await inquirer.prompt([
+            {
+                type:"input",
+                name: "name",
+                message: "Enter Team Member's name."
+            },
+            {
+                type:"input",
+                name: "id",
+                message: "Enter Team Member's ID."
+            },
+            {
+                type:"input",
+                name: "email",
+                message: "Enter Team Member's Email"
+            },
+            {
+                type:"list",
+                name: "role",
+                message: "What is your role?",
+                choices: ["Manager", "Engineer", "Intern"]
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Enter the employee's GitHub username.",
+                when: answer => answer.role === "Engineer"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Enter office number.",
+                when: answer => answer.role === "Manager"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Enter the employee's school.",
+                when: answer => answer.role === "Intern"
+            }
+
+        ]);
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function init() {
+        
+    const responses = await inquirer.prompt(questions);
+        
+    let team = [];
+
+
+
+
+        const readMe = generateMarkdown(responses)
+        await writeFileAsync("README.md", readMe);
     
+};
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
